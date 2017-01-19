23fd3e96065dc3efeaa850bba9c1a2e3cfe2d44b
Mak Sò <mak.so1979@gmail.com>
2017-01-19 23:07:06 +0100

    shit

--- src/index.js
+++ src/index.js
@@ -105,11 +105,11 @@ class YaarhLib {
   handler(event, lambdaContext, callback) {
     console.log('YaarhLogs: Processing event:', event)
     const method = event.httpMethod.toLowerCase()
-    console.log('YaarhLogs: Path match found?', exist)
     this._currentEvent = Object.assign({}, event, { lambdaContext })
     this._callback = callback
     
     const executed = this._routes[method].run('/'+event.pathParameters.proxy)
+    console.log('YaarhLogs: Path match found?', executed)
     if(!executed){
       this._callback(null, {
         statusCode: 404,
